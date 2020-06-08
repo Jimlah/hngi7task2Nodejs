e4bf362b2558a744b41436bc16f541ebcd82f143
AK <mywork304@gmail.com>
2020-06-08 17:46:08 +0200

    adding node start structure

---
+++ app.js
@@ -0,0 +1,16 @@
+const express=require('express');
+const app = express();
+const routes=require('./routes')
+const PORT=3000;
+
+app.use(routes);
+
+
+
+
+
+
+
+app.listen(process.env.PORT|PORT,()=>{
+    console.log(`server is up on port ${PORT} `)
+})
