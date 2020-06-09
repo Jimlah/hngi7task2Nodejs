d10b94e5242f278303aa32935b6fea16797b5978
AK <mywork304@gmail.com>
2020-06-09 12:24:12 +0200

    add cors ,renameEndpoint

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express=require('express');
 const app = express();
 const routes=require('./routes')
-
+const cors = require('cors')
 const PORT=3000;
-
+app.use(cors())
 app.use(routes);
 
 
