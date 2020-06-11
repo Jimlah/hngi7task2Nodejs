04261afe40b44a454d574aa6b4d86b662720763e
youngxian <38491485+youngxian@users.noreply.github.com>
2020-06-11 16:06:26 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 git pull origin
-docker stop teamflashnodejs && docker rm teamflashnodejs && docker rmi teamflashnodejs:latest
+docker stop teamflashnodejs && docker rm teamflashnodejs
+#docker rmi teamflashnodejs:latest
+docker build --tag teamflashnodejs .
 docker run -p 1050:3000 -d --name teamflashnodejs teamflashnodejs:latest
